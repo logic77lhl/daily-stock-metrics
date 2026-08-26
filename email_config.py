50dfe553e0a8472e0f266d2551fa290f4730bfea
hailong <543517307@qq.com>
2026-08-26 18:38:36 +0800

    email_config 空环境变量时回退默认值

--- email_config.py
+++ email_config.py
@@ -1,7 +1,7 @@
 import os
 
-SMTP_SERVER = os.environ.get("SMTP_SERVER", "smtp.qq.com")
-SMTP_PORT = int(os.environ.get("SMTP_PORT", "465"))
-SENDER_EMAIL = os.environ.get("SENDER_EMAIL", "")
-SENDER_AUTH_CODE = os.environ.get("SENDER_AUTH_CODE", "")
-RECIPIENT_EMAIL = os.environ.get("RECIPIENT_EMAIL", "")
+SMTP_SERVER = os.environ.get("SMTP_SERVER") or "smtp.qq.com"
+SMTP_PORT = int(os.environ.get("SMTP_PORT") or 465)
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL") or ""
+SENDER_AUTH_CODE = os.environ.get("SENDER_AUTH_CODE") or ""
+RECIPIENT_EMAIL = os.environ.get("RECIPIENT_EMAIL") or ""
